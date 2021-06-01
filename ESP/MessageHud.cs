cd5b98763877d6b2cefa344a192f420b91ce0845
Jere Kuusela <jere.kuusela@iki.fi>
2021-06-01 23:34:48 +0300

    Fixed errors st start up

--- ESP/MessageHud.cs
+++ ESP/MessageHud.cs
@@ -6,7 +6,7 @@ namespace ESP
   [HarmonyPatch(typeof(MessageHud), "UpdateMessage")]
   public class MessageHud_UpdateMessage
   {
-    private static string forcedMessage;
+    private static string forcedMessage = "";
     public static string ForcedMessage
     {
       get
@@ -15,18 +15,27 @@ namespace ESP
       }
       set
       {
-        if (value.Length == 0)
-          MessageHud.instance.m_messageText.CrossFadeAlpha(0f, 4f, true);
-        else
-          MessageHud.instance.m_messageText.CrossFadeAlpha(1f, 0f, true);
+        if (MessageHud.instance && MessageHud.instance.m_messageText)
+        {
+          if (value.Length == 0)
+            MessageHud.instance.m_messageText.CrossFadeAlpha(0f, 4f, true);
+          else
+            MessageHud.instance.m_messageText.CrossFadeAlpha(1f, 0f, true);
+        }
         forcedMessage = value;
       }
     }
 
     public static bool Prefix()
     {
-      if (ForcedMessage.Length == 0) return true;
-      MessageHud.instance.m_messageText.text = ForcedMessage;
+      var hud = MessageHud.instance;
+      if (ForcedMessage.Length == 0 || !hud) return true;
+      if (!hud.m_messageText)
+      {
+        hud.ShowMessage(MessageHud.MessageType.TopLeft, ForcedMessage);
+        return true;
+      }
+      hud.m_messageText.text = ForcedMessage;
       return false;
     }
   }
